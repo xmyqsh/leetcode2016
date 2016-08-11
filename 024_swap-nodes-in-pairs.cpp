53d9dd105a225fbae800f02bd07d5fc4ebbdeb2d
xmyqsh <xmyqsh@gmail.com>
2016-08-12 00:30:13 +0800

    024

---
+++ 024_swap-nodes-in-pairs.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* newHead = head->next;
+        head->next = swapPairs(newHead->next);
+        newHead->next = head;
+        return newHead;
+    }
+};
