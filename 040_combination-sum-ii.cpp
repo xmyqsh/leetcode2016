1d1e7901d4dc864857d07b03e4552556d7971845
xmyqsh <xmyqsh@gmail.com>
2016-08-12 22:55:26 +0800

    040

---
+++ 040_combination-sum-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int> > result;
+        vector<int> path;
+        sort(candidates.begin(), candidates.end());
+        travel(&result, &path, candidates, target, 0, 0);
+        return result;
+    }
+    void travel(vector<vector<int> >* result, vector<int>* path, const vector<int>& candidates, int target, int sum, int cur) {
+        if (target == sum) {
+            (*result).push_back(*path);
+            return;
+        }
+        if (target < sum) return;
+        for (int i = cur; i < candidates.size(); ++i) {
+            if (i != cur && candidates[i] == candidates[i - 1]) continue;
+            (*path).push_back(candidates[i]);
+            travel(result, path, candidates, target, sum + candidates[i], i + 1);
+            (*path).pop_back();
+        }
+    }
+};
