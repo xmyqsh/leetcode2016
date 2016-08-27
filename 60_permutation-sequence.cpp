347310b4e47e8ca61014067c57f4afc6395ab884
xmyqsh <xmyqsh@gmail.com>
2016-08-27 23:01:11 +0800

    60

---
+++ 60_permutation-sequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        stringstream ss;
+        int cnt = 1;
+        for (int i = 1; i <= n; ++i) {
+            cnt *= i;
+            ss << i;
+        }
+        string s = ss.str();
+        int sum, j;
+        for (int i = 0; i < n; ++i) {
+            cnt /= n - i;
+            for (sum = 0, j = 1; i + j <= n; ++j) {
+                if (sum + cnt >= k) break;
+                sum += cnt;
+                swap(s[i], s[i + j]);
+            }
+            k -= sum;
+        }
+        return s;
+    }
+};
