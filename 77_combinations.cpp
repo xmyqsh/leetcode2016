b1dfa04f7f056640dcbcd080f381c131a760b7e1
xmyqsh <xmyqsh@gmail.com>
2016-08-30 07:57:56 +0800

    77

---
+++ 77_combinations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int> > result;
+        vector<int> path;
+        travel(&result, path, n, k, 1);
+        return result;
+    }
+    void travel(vector<vector<int> >* result, vector<int>& path, int n, int k, int cur) {
+        if (path.size() == 2) {
+            result->push_back(path);
+            return;
+        }
+        for (int i = cur; i <= n; ++i) {
+            path.push_back(i);
+            travel(result, path, n, k, i + 1);
+            path.pop_back();
+        }
+    }
+};
