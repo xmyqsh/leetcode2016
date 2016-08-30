0773818a5cacbbba69f7d746dadf0742462ea7d0
xmyqsh <xmyqsh@gmail.com>
2016-08-30 17:08:26 +0800

    recursive + pruning

--- 87_scramble-string.cpp
+++ 87_scramble-string.cpp
@@ -19,3 +19,31 @@ public:
         return dp[0][0][s1.size()];
     }
 };
+// recursive + pruning
+class Solution {
+private:
+    typedef string::iterator Iterator;
+public:
+    bool isScramble(string s1, string s2) {
+        if (s1.size() != s2.size()) return false;
+        if (s1.empty()) return true;
+        return isScramble(s1.begin(), s1.end(), s2.begin());
+    }
+private:
+    bool isScramble(Iterator first1, Iterator last1, Iterator first2) {
+        auto length = distance(first1, last1);
+        auto last2 = next(first2, length);
+        if (length == 1) return *first1 == *first2;
+        auto tmp1 = first1, tmp2 = first2;
+        vector<int> count(26, 0);
+        for (int i = 0; i < length; ++i)
+            count[*tmp1++ - 'a']++, count[*tmp2++ - 'a']--;
+        for (int i = 0; i < 26; ++i) if (count[i]) return false;
+        for (int i = 1; i < length; ++i) {
+            if ((isScramble(first1, first1 + i, first2) && isScramble(first1 + i, last1, first2 + i)) || 
+                (isScramble(first1, first1 + i, last2 - i) && isScramble(first1 + i, last1, first2)))
+                return true;
+        }
+        return false;
+    }
+};
