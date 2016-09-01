1e7b419cca0c6df09e3edc59956262af85feb478
xmyqsh <xmyqsh@gmail.com>
2016-09-01 22:08:13 +0800

    142

---
+++ 142_linked-list-cycle-ii.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next || !head->next->next) return NULL;
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) break;
+        }
+        if (slow != fast) return NULL;
+        for (fast = head; slow != fast; slow = slow->next, fast = fast->next);
+        return slow;
+    }
+};
