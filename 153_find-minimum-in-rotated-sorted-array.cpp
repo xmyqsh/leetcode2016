177c7ec98143c5d7a3f3852c9d6bdc8edbdac16c
xmyqsh <xmyqsh@gmail.com>
2016-09-02 17:43:42 +0800

    153

---
+++ 153_find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int b = 0, e = nums.size() - 1;
+        while (b < e) {
+            const int mid = b + (e - b) / 2;
+            if (nums[mid] > nums[e]) b = mid + 1;
+            else e = mid;
+        }
+        return nums[b];
+    }
+};
