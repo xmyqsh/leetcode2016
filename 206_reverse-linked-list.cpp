e18a49cf068ed8fcf126c701313d4a8a11758093
xmyqsh <xmyqsh@gmail.com>
2016-09-03 16:52:38 +0800

    206

--- 206_reverse-linked-list.cpp
+++ 206_reverse-linked-list.cpp
@@ -21,3 +21,23 @@ public:
         return prev;
     }
 };
+
+// recursively
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
