2cdd356fb3d985735ad85d9c5d4c70930c8cf482
xmyqsh <xmyqsh@gmail.com>
2016-09-04 17:25:26 +0800

    poor improvement

--- 214_shortest-palindrome.cpp
+++ 214_shortest-palindrome.cpp
@@ -30,3 +30,25 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int b = 0, e = s.size() - 1;
+        while (b < e)
+            if (s[b] != s[e]) break;
+            else { ++b; --e; }
+        if (b >= e) return s;
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        int n = s.size(), k, i, j;
+        for (k = 0; k < n; ++k) {
+            if (s2.substr(k) == s.substr(0, n - k)) break;
+            /*  // time out.  why is this slower than substr ?
+            for (i = k, j = 0; i < n && s2[i] == s[j]; ++i, ++j);
+            if (i == n) break;
+            */
+        }
+        return s2.substr(0, k).append(s);
+    }
+};
