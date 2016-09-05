8571ee5a6b90c1c376d9315e2957c092aa20aa44
xmyqsh <xmyqsh@gmail.com>
2016-09-06 00:13:46 +0800

    225

---
+++ 225_implement-stack-using-queues.cpp
@@ -0,0 +1,29 @@
+class Stack {
+public:
+    // Push element x onto stack.
+    void push(int x) {
+        q2.push(x);
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1, q2);
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {
+        q1.pop();
+    }
+
+    // Get the top element.
+    int top() {
+        return q1.front();
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {
+        return q1.empty();
+    }
+private:
+    queue<int> q1, q2;
+};
