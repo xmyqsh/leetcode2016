43666885cd8ba7f663381e834b06a597bfbe1126
xmyqsh <xmyqsh@gmail.com>
2016-09-06 10:15:06 +0800

    231

---
+++ 231_power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n < 1) return false;
+        if (n == 1) return true;
+        while (n != 2) {
+            if (n % 2) return false;
+            n /= 2;
+        }
+        return true;
+    }
+};
