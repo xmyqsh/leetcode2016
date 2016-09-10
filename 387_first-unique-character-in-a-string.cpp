2df88457c61ba126cf20971d36838c2558b0e79a
xmyqsh <xmyqsh@gmail.com>
2016-09-10 22:44:34 +0800

    387_first-unique-character-in-a-string.cpp

---
+++ 387_first-unique-character-in-a-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> count(256, 0);
+        for (auto c : s) ++count[c];
+        for (int i = 0; i != s.size(); ++i) if (count[s[i]] == 1) return i;
+        return -1;
+    }
+};
