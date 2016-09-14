d859653bc80ae2353606d264d5bb9af8036e0168
xmyqsh <xmyqsh@gmail.com>
2016-09-14 10:26:50 +0800

    DP version

--- 343_integer-break.cpp
+++ 343_integer-break.cpp
@@ -11,3 +11,17 @@ public:
         return 0;
     }
 };
+
+//DP
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        vector<int> dp(n + 1, 0);
+        dp[2] = 1;
+        for (int i = 3; i <= n; ++i)
+            for (int j = 2; j < i; ++j)
+                dp[i] = max(dp[i], j * max(i - j, dp[i - j]));
+        return dp[n];
+    }
+};
