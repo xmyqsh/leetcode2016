076e0be1d749fc83ffeb237890c38b2777fede81
xmyqsh <xmyqsh@gmail.com>
2016-09-17 11:24:18 +0800

    O(n) solution need some proof

--- 376_wiggle-subsequence.cpp
+++ 376_wiggle-subsequence.cpp
@@ -12,3 +12,17 @@ public:
         return max(dp[nums.size() - 1][0], dp[nums.size() - 1][1]);
     }
 };
+
+// O(n) solution is tricky
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        vector<vector<int> > dp(nums.size(), vector<int>(2, 1));
+        for (int i = 1; i != nums.size(); ++i) {
+            dp[i][0] = max(dp[i - 1][0], nums[i - 1] > nums[i] ? dp[i - 1][1] + 1 : dp[i - 1][0]);
+            dp[i][1] = max(dp[i - 1][1], nums[i - 1] < nums[i] ? dp[i - 1][0] + 1 : dp[i - 1][1]);
+        }
+        return max(dp[nums.size() - 1][0], dp[nums.size() - 1][1]);
+    }
+};
