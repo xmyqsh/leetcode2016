dd54350ea3668c6df2b8da1d93d33d9354f02bfa
xmyqsh <xmyqsh@gmail.com>
2016-10-03 10:55:04 +0800

    390

--- 390_elimination-game.cpp
+++ 390_elimination-game.cpp
@@ -12,3 +12,11 @@ public:
         return start;
     }
 };
+
+// recur
+class Solution {
+public:
+    int lastRemaining(int n) {
+        return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+    }
+};
