dc034eef34403b22f30f22885e25b7f236b48837
xmyqsh <xmyqsh@gmail.com>
2016-10-06 11:02:33 +0800

    O(n) solution

--- 402_remove-k-digits.cpp
+++ 402_remove-k-digits.cpp
@@ -1,9 +1,10 @@
+// O(k * n)
 class Solution {
 public:
     string removeKdigits(string num, int k) {
         if (num.size() == k) return "0";
         for (int i = 0, j; i != k; ++i) {
-            for (j = 1; num[j - 1] <= num[j] && j != num.size(); ++j);
+            for (j = 1; j != num.size() && num[j - 1] <= num[j]; ++j);
             if (j != num.size()) num.erase(j - 1, 1);
             else num.erase(num.size() - 1);
         }
@@ -11,5 +12,32 @@ public:
         while (num[i] == '0') ++i;
         num.erase(0, i);
         return num.empty() ? "0" : num;
+    }
+};
+
+// O(n)
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> sk; int i;
+        for (i = 0; k && i != num.size(); ++i) {
+            while (k && !sk.empty() && sk.top() > num[i]) {
+                sk.pop();
+                --k;
+            }
+            sk.push(num[i]);
+        }
+        stringstream ss; string result;
+        while (!sk.empty()) {
+            ss << sk.top();
+            sk.pop();
+        }
+        result = ss.str();
+        reverse(result.begin(), result.end());
+        result.append(num.substr(i, (int)num.size() - i));
+        result.erase((int)result.size() - k);
+        for (i = 0; i != result.size() && result[i] == '0'; ++i);
+        result.erase(0, i);
+        return result.empty() ? "0" : result;
     }
 };
