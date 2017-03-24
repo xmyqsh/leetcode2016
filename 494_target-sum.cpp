60af5298181d22f2e3d48d24e584fed6513db3c5
xmyqsh <xmyqsh@gmail.com>
2017-03-24 11:40:19 +0800

    optimized dp

--- 494_target-sum.cpp
+++ 494_target-sum.cpp
@@ -39,3 +39,19 @@ public:
         return f[sum + S];
     }
 };
+// optimized dp
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum < S || (sum + S) & 1) return 0;
+        // sum + S = 2 * sum(P);
+        vector<int> dp(((sum + S) >> 1) + 1, 0);
+        dp[0] = 1;
+        for (auto n : nums) {
+            for (int i = dp.size() - 1; i >= n; --i)
+                dp[i] += dp[i - n];
+        }
+        return dp[dp.size() - 1];
+    }
+};
