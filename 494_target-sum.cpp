77718dead2848f35d3055a6acdbb74dfada6baba
xmyqsh <xmyqsh@gmail.com>
2017-03-24 11:29:39 +0800

    dp

--- 494_target-sum.cpp
+++ 494_target-sum.cpp
@@ -1,3 +1,4 @@
+// dfs
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
@@ -13,5 +14,28 @@ public:
         }
         dfs(ret, nums, S, cur + nums[idx], idx + 1);
         dfs(ret, nums, S, cur - nums[idx], idx + 1);
+    }
+};
+
+// dp
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if (sum < S) return 0;
+        return dp(nums, S, sum);
+    }
+    int dp(const vector<int>& nums, int S, int sum) {
+        vector<int> f(2*sum + 1, 0);
+        f[sum] = 1;
+        for (auto n : nums) {
+            vector<int> f1(2*sum + 1, 0);
+            for (int i = 0; i != f.size(); ++i) {
+                if (i + n < f.size()) f1[i + n] += f[i];
+                if (i - n >= 0) f1[i - n] += f[i];
+            }
+            swap(f1, f);
+        }
+        return f[sum + S];
     }
 };
