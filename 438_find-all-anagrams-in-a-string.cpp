025f846ebac28bfb37c04b958637f74ee84c76e0
xmyqsh <xmyqsh@gmail.com>
2017-05-10 21:08:42 +0800

    sliding window with vector

--- 438_find-all-anagrams-in-a-string.cpp
+++ 438_find-all-anagrams-in-a-string.cpp
@@ -26,3 +26,24 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> result;
+        if (p.size() > s.size()) return result;
+        vector<int> mp(26, 0), ms(26, 0);
+        int i = 0;
+        for (; i != p.size(); ++i) {
+            ++mp[p[i] - 'a'];
+            ++ms[s[i] - 'a'];
+        }
+        if (mp == ms) result.push_back(0);
+        for (; i != s.size(); ++i) {
+            --ms[s[i - p.size()] - 'a'];
+            ++ms[s[i] - 'a'];
+            if (mp == ms) result.push_back(i - p.size() + 1);
+        }
+        return result;
+    }
+};
