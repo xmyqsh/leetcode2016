c4220c668e813d996689eb40eab4be3b3472a3c3
xmyqsh <xmyqsh@gmail.com>
2017-05-31 17:54:33 +0800

    update api

--- 139_word-break.cpp
+++ 139_word-break.cpp
@@ -1,21 +1,62 @@
 class Solution {
-public:
-    bool wordBreak(string s, unordered_set<string>& wordDict) {
-        vector<bool> f(s.size() + 1, false);
-        f[0] = true;
-        int minLen = INT_MAX, maxLen = INT_MIN;
-        for (auto word : wordDict) {
-            minLen = min(minLen, (int)word.length());
-            maxLen = max(maxLen, (int)word.length());
+private:
+    class TrieTree {
+    private:
+        class TrieNode {
+        public:
+            TrieNode* next[26];
+            bool have;
+        public:
+            TrieNode() {
+                for (int i = 0; i != 26; ++i) this->next[i] = NULL;
+                this->have = false;
+            }
+        };
+    private:
+        TrieNode* root;
+    public:
+        TrieTree() {
+            this->root = new TrieNode();
         }
-        for (int i = 1; i <= s.size(); ++i) {
-            for (int j = minLen, pos = i - minLen; j <= maxLen && pos >= 0; ++j, --pos) {
-                if (f[pos] && wordDict.count(s.substr(pos, j))) {
-                    f[i] = true;
+        void insert(const string& s) {
+            TrieNode* cur = root;
+            for (auto ch : s) {
+                int idx = ch - 'a';
+                if (!cur->next[idx])
+                    cur->next[idx] = new TrieNode();
+                cur = cur->next[idx];
+            }
+            cur->have = true;
+        }
+        bool search(const string& s) {
+            TrieNode* cur = root;
+            for (auto ch : s) {
+                int idx = ch - 'a';
+                if (!cur->next[idx]) return false;
+                cur = cur->next[idx];
+            }
+            return cur->have;
+        }
+    };
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int minLen = INT_MAX, maxLen = 0;
+        TrieTree* tree = new TrieTree();
+        for (auto& word : wordDict) {
+            minLen = min(minLen, (int)word.size());
+            maxLen = max(maxLen, (int)word.size());
+            tree->insert(word);
+        }
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+        for (int i = 1; i != s.size() + 1; ++i) {
+            for (int j = minLen, pos = i - j; j != maxLen + 1 && pos >= 0; ++j, --pos) {
+                if (dp[pos] && tree->search(s.substr(pos, j))) {
+                    dp[i] = true;
                     break;
                 }
             }
         }
-        return f[s.size()];
+        return dp[s.size()];
     }
 };
