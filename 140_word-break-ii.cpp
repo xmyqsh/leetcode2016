c9b6103b35451e625ef663008acccb195b08c643
xmyqsh <xmyqsh@gmail.com>
2017-06-01 15:59:15 +0800

    update api, dp  backtracking

--- 140_word-break-ii.cpp
+++ 140_word-break-ii.cpp
@@ -1,39 +1,83 @@
 class Solution {
-public:
-    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
-        vector<vector<bool> > wordPos(s.size() + 1, vector<bool>(s.size(), false));
-        vector<bool> f(s.size() + 1, false);
-        f[0] = true;
-        int minLen = INT_MAX, maxLen = INT_MIN;
-        for (auto word : wordDict) {
-            minLen = min(minLen, (int)word.length());
-            maxLen = max(maxLen, (int)word.length());
+private:
+    class TrieTree {
+    private:
+        class TrieNode {
+        public:
+            TrieNode* next[26];
+            bool have;
+        public:
+            TrieNode() {
+                for (int i = 0; i != 26; ++i)
+                    this->next[i] = NULL;
+                this->have = false;
+            }
+        };
+    private:
+        TrieNode* root;
+    public:
+        TrieTree() {
+            this->root = new TrieNode();
         }
-        for (int i = 1; i <= s.size(); ++i) {
-            for (int j = minLen, pos = i - minLen; j <= maxLen && pos >= 0; ++j, --pos) {
-                if (f[pos] && wordDict.count(s.substr(pos, j))) {
-                    f[i] = true;
-                    wordPos[i][pos] = true;
+        void insert(const string& s) {
+            TrieNode* cur = root;
+            for (auto c : s) {
+                int idx = c - 'a';
+                if (!cur->next[idx])
+                    cur->next[idx] = new TrieNode();
+                cur = cur->next[idx];
+            }
+            cur->have = true;
+        }
+        bool search(const string& s) {
+            TrieNode* cur = root;
+            for (auto c : s) {
+                int idx = c - 'a';
+                if (!cur->next[idx]) return false;
+                cur = cur->next[idx];
+            }
+            return cur->have;
+        }
+    };
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        TrieTree* tree = new TrieTree();
+        int minLen = INT_MAX, maxLen = 0;
+        for (auto& word : wordDict) {
+            minLen = min(minLen, (int)word.size());
+            maxLen = max(maxLen, (int)word.size());
+            tree->insert(word);
+        }
+        vector<bool> dp(s.size() + 1, false);
+        vector<vector<bool> > wordPos(s.size(), vector<bool>(maxLen + 1, false));
+        dp[0] = true;
+        for (int i = 1; i != s.size() + 1; ++i) {
+            for (int j = minLen, pos = i - j; j != maxLen + 1 && pos >= 0; ++j, --pos) {
+                if (dp[pos] && tree->search(s.substr(pos, j))) {
+                    dp[i] = true;
+                    wordPos[pos][j] = true;
                 }
             }
         }
         vector<string> result;
+        if (!dp[s.size()]) return result;
         vector<string> path;
-        getPath(&result, wordPos, path, s, s.size());
+        genPath(result, path, wordPos, s, 0, minLen, maxLen);
         return result;
     }
-    void getPath(vector<string>* result, vector<vector<bool> >& wordPos, vector<string>& path, const string& s, int cur) {
-        if (cur == 0) {
+    void genPath(vector<string>& result, vector<string>& path, const vector<vector<bool> >& wordPos,
+                    const string& s, int cur, int minLen, int maxLen) {
+        if (cur == s.size()) {
             stringstream ss;
-            for (int i = path.size() - 1; i > 0; --i) ss << path[i] << " ";
-            ss << path[0];
-            result->push_back(ss.str());
+            for (int i = 0; i != path.size() - 1; ++i) ss << path[i] << ' ';
+            ss << path[path.size() - 1];
+            result.push_back(ss.str());
             return;
         }
-        for (int i = cur - 1; i >= 0; --i) {
-            if (wordPos[cur][i]) {
-                path.push_back(s.substr(i, cur - i));
-                getPath(result, wordPos, path, s, i);
+        for (int j = minLen; j != maxLen + 1; ++j) {
+            if (wordPos[cur][j]) {
+                path.push_back(s.substr(cur, j));
+                genPath(result, path, wordPos, s, cur + j, minLen, maxLen);
                 path.pop_back();
             }
         }
