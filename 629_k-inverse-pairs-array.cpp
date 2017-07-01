88078bf53a95d800fbc894915f948420507c1ab2
xmyqsh <xmyqsh@gmail.com>
2017-07-01 15:12:04 +0800

    memorization version

--- 629_k-inverse-pairs-array.cpp
+++ 629_k-inverse-pairs-array.cpp
@@ -39,3 +39,21 @@ public:
         return dp[k];
     }
 };
+
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        MOD = 1e9 + 7;
+        int cache[1001][1001] = {0};
+        return (calc(n, k, cache) + MOD - (k != 0 ? calc(n, k - 1, cache) : 0)) % MOD;
+    }
+    int calc(int n, int k, int cache[][1001]) {
+        if (n == 0) return 0;
+        if (k == 0) return 1;
+        if (cache[n][k]) return cache[n][k];
+        int val = (calc(n - 1, k, cache) + MOD - (k >= n ? calc(n - 1, k - n, cache) : 0)) % MOD;
+        return cache[n][k] = (calc(n, k - 1, cache) + val) % MOD;
+    }
+public:
+    int MOD;
+};
