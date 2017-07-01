d436089337f5e284bde15d1dffd65042081419f9
xmyqsh <xmyqsh@gmail.com>
2017-07-01 11:16:43 +0800

    dp with optimization

--- 629_k-inverse-pairs-array.cpp
+++ 629_k-inverse-pairs-array.cpp
@@ -18,3 +18,24 @@ public:
         return (dp[n][k] + M - dp[n][k - 1]) % M;
     }
 };
+
+class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        vector<int> dp(k + 1, 0);
+        const int M = 1e9 + 7;
+        for (int i = 1; i != n + 1; ++i) {
+            vector<int> tmp(k + 1, 0);
+            for (int j = 0; j <= min(k, i * (i - 1) / 2); ++j) {
+                if (j == 0) {
+                    tmp[j] = 1;
+                } else {
+                    int val = (dp[j] + M - (j >= i ? dp[j - i] : 0)) % M;
+                    tmp[j] = (tmp[j - 1] + val) % M;
+                }
+            }
+            swap(dp, tmp);
+        }
+        return dp[k];
+    }
+};
