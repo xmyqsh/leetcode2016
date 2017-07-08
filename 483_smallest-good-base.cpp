d9c13b1acf46f7f2d8a80a1283bcc5d339b9d875
xmyqsh <xmyqsh@gmail.com>
2017-07-08 17:23:57 +0800

    math

---
+++ 483_smallest-good-base.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+        unsigned long long num = stoll(n);
+        for (int m = log((double)num) / log((double)2); m != 1; --m) {
+            unsigned long long base = pow((double)num, (double)1 / m);
+            if (myPow(base, m + 1) - 1 == num * (base - 1))
+                return to_string(base);
+        }
+        return to_string(num - 1);
+    }
+    unsigned long long myPow(unsigned long long base, int m) {
+        unsigned long long result = base;
+        for (int i = 1; i != m; ++i) result *= base;
+        return result;
+    }
+};
+
+/*
+n = 1 + b + b^2 + ... + b^m
+n - 1 = b * (1 + b + ... + b^(m - 1))
+n - b^m = 1 + b + ... + b^(m - 1)
+n - 1 = b * (n - b^m)
+(b - 1) * n = b^(m + 1) - 1
+n = (b^(m + 1) - 1) / (b - 1)
+
+m = math.log(n, base)
+base = math.pow(n, (double)1 / m)
+*/
