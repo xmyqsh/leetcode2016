d48d05b7c1319fa6ecf3387a2cf97e4430c77577
xmyqsh <xmyqsh@gmail.com>
2017-08-01 20:25:23 +0800

    set

---
+++ 575_distribute-candies.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        return min(len(set(candies)), len(candies) / 2)
