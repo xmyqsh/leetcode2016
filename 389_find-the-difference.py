5d7b446251dce9b6419ff0c36036a91965c2dad6
xmyqsh <xmyqsh@gmail.com>
2017-08-02 19:22:41 +0800

    reduce map operator.xor ord

---
+++ 389_find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        return chr(reduce(operator.xor, map(ord, s + t)))
