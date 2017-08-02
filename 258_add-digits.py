ffb1e14f1e5bf28c2506adcee7eec5dfbdb88654
xmyqsh <xmyqsh@gmail.com>
2017-08-02 18:44:48 +0800

    mod

---
+++ 258_add-digits.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0: return 0
+        return (num - 1) % 9 + 1
