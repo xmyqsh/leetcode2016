426902bbee9e64f378a2381f78d5018e760985f1
xmyqsh <xmyqsh@gmail.com>
2017-08-02 19:14:00 +0800

    tree string format

--- 606_construct-string-from-binary-tree.py
+++ 606_construct-string-from-binary-tree.py
@@ -21,3 +21,21 @@ class Solution(object):
             return ''.join([str(t.val), '(', left, ')'])
         else:
             return str(t.val)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        if not t: return ''
+        left = '({})'.format(self.tree2str(t.left)) if (t.left or t.right) else ''
+        right = '({})'.format(self.tree2str(t.right)) if (t.right) else ''
+        return '{}{}{}'.format(t.val, left, right)
