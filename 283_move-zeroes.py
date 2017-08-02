658b2749202d386574d19c0c0a92e22032db5bdc
xmyqsh <xmyqsh@gmail.com>
2017-08-02 19:30:14 +0800

    sort lambda

---
+++ 283_move-zeroes.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        nums.sort(key = lambda x: 1 if x == 0 else 0)
