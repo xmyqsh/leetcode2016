d8599dce85e231ce12f406de43d3369de7d8e4e3
xmyqsh <xmyqsh@gmail.com>
2017-08-05 11:12:08 +0800

    dfs map tuple

--- 652_find-duplicate-subtrees.cpp
+++ 652_find-duplicate-subtrees.cpp
@@ -26,3 +26,33 @@ public:
         return key;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    map<tuple<int, int, int>, int> mp;
+    unordered_map<int, int> cnt;
+    vector<TreeNode*> result;
+    int id;
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        id = 1;
+        dfs(root);
+        return result;
+    }
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+        tuple<int, int, int> tmp = {root->val, dfs(root->left), dfs(root->right)};
+        if (!mp.count(tmp)) mp[tmp] = id++;
+        if (++cnt[mp[tmp]] == 2) result.push_back(root);
+        return mp[tmp];
+    }
+};
