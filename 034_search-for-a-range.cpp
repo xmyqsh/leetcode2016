c514865ad38ca06555682c4f792e9fa8f844fa5a
xmyqsh <xmyqsh@gmail.com>
2020-04-02 12:07:48 +0800

    implement official lower_bound and upper_bound

--- 034_search-for-a-range.cpp
+++ 034_search-for-a-range.cpp
@@ -23,3 +23,29 @@ public:
         else return nums[e] == target ? b - 1 : -1;
     }
 };
+
+// implement official lower_bound and upper_bound
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {lower_bound(nums, target), upper_bound(nums, target)};
+    }
+    int lower_bound(const vector<int>& nums, int target) {
+        int b = 0, e = nums.size();
+        while (b != e) {
+            const int mid = b + (e - b) / 2;
+            if (nums[mid] < target) b = mid + 1;
+            else e = mid;
+        }
+        return b != nums.size() && nums[b] == target ? b : -1;
+    }
+    int upper_bound(const vector<int>& nums, int target) {
+        int b = -1, e = nums.size() - 1;
+        while (b != e) {
+            const int mid = b + (e - b + 1) / 2;
+            if (nums[mid] > target) e = mid - 1;
+            else b = mid;
+        }
+        return e != -1 && nums[e] == target ? e : -1;
+    }
+};
