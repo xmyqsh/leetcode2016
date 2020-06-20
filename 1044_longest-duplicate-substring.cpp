9c9b727e7ef717e426a61f7ea7856de6fea8e3d4
xmyqsh <xmyqsh@gmail.com>
2020-06-20 09:21:15 +0800

    fix potential overflow bug

--- 1044_longest-duplicate-substring.cpp
+++ 1044_longest-duplicate-substring.cpp
@@ -4,7 +4,7 @@ public:
         unordered_set<unsigned long long> st;
         unsigned long long RKHash = w == 1 ? 0 : RKHashCache[w - 2];
         for (int i = w - 1; i != S.size(); ++i) {
-            RKHash = ((RKHash * 256) % 28379606267245447 + (S[i] - 'a')) % 28379606267245447;
+            RKHash = ((RKHash * 26) % 28379606267245447 + (S[i] - 'a')) % 28379606267245447;
             if (st.count(RKHash)) return i - (w - 1);
             st.insert(RKHash);
             RKHash += 28379606267245447 - ((S[i - (w - 1)] - 'a') * magic[w - 1]) % 28379606267245447;
@@ -16,8 +16,8 @@ public:
         vector<unsigned long long> magic(S.size(), 0); magic[0] = 1;
         vector<unsigned long long> RKHashCache(S.size(), -1); RKHashCache[0] = S[0] - 'a';
         for (int i = 1; i != S.size(); ++i) {
-            magic[i] = (magic[i - 1] * 256) % 28379606267245447;
-            RKHashCache[i] = ((RKHashCache[i - 1] * 256) % 28379606267245447 + (S[i] - 'a')) % 28379606267245447;
+            magic[i] = (magic[i - 1] * 26) % 28379606267245447;
+            RKHashCache[i] = ((RKHashCache[i - 1] * 26) % 28379606267245447 + (S[i] - 'a')) % 28379606267245447;
         }
         int b = 0, e = S.size() - 1;
         while (b != e) {
