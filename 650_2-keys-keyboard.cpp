8224aecce92233b9bfb9d41741a9bd121eef45ca
xmyqsh <xmyqsh@gmail.com>
2020-06-24 13:57:10 +0800

    A straight forward solution

--- 650_2-keys-keyboard.cpp
+++ 650_2-keys-keyboard.cpp
@@ -26,3 +26,25 @@ public:
         return result;
     }
 };
+
+// A straight forword solution
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n + 1, INT_MAX); dp[1] = 0;
+        for (int i = 2; i != n + 1; ++i) {
+            //for (int j = i - 1; j != 0; --j) {
+            for (int j = i / 2; j != 0; --j) {
+                if ((i - j) % j == 0) dp[i] = min(dp[i], dp[j] + 1 + (i - j) / j);
+            }
+        }
+        return dp[n];
+    }
+};
+
+/*
+3:  A --> AA --> AAA                                                                    A
+7:  A --> AA --> AAA --> AAAA --> AAAAA --> AAAAAA --> AAAAAAA                          A
+10: A --> AA --> AAAA --> AAAAAA --> AAAAAAAA --> AAAAAAAAAA                            AA
+24: A --> AA --> AAAA --> AAAAAAAA --> AAAAAAAAAAAAAAAA --> AAAAAAAAAAAAAAAAAAAAAAAA    AAAAAAAA
+*/
