d12b00c8a1b5837fc59218e68d069f82b524392c
xmyqsh <xmyqsh@gmail.com>
2020-06-25 18:51:07 +0800

    special dp

--- 956_tallest-billboard.cpp
+++ 956_tallest-billboard.cpp
@@ -1,1 +1,22 @@
-// TODO
+// O(N*S)
+class Solution {
+public:
+    int tallestBillboard(vector<int>& rods) {
+        unordered_map<int, int> sum2len;
+        sum2len[0] = 0;
+        for (auto rod : rods) {
+            unordered_map<int, int> update;
+            for (const auto& item : sum2len) {
+                auto sum = item.first, len = item.second;
+                // add to left
+                update[sum + rod] = max(update[sum + rod], len + rod);
+                // add to right
+                update[sum - rod] = max(update[sum - rod], len);
+                // discard
+                update[sum]       = max(update[sum],       len);
+            }
+            swap(sum2len, update);
+        }
+        return sum2len[0];
+    }
+};
